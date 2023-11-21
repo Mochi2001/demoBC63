8abfd9a960d80577c73776db58aedec7e3400cba
Mochi2001 <thienvy2001201@gmail.com>
2023-11-21 19:02:54 +0700

    test

--- Bản sao Theme light vs dark/script.js
+++
@@ -1,9 +0,0 @@
-function toggleTheme() {
-    const body = document.body;
-    body.classList.toggle("light-theme");
-    body.classList.toggle("dark-theme");
-
-    // Đối với phần tử cần ẩn hoặc hiển thị
-    const specialBox = document.getElementById("special-box");
-    specialBox.classList.toggle("hidden");
-}
